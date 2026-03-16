effe7ab4d54ce91ef4c4a6bbd48b023108b086ac
Liu <ljintao08@gmail.com>
2026-03-16 11:12:30 +0800

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Game from './pages/Game';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from "./components/ui/sonner.jsx"
+import { Analytics } from "@vercel/analytics/next"
 
 const queryClient = new QueryClient();
 
